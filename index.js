616be4b588af1ce51f664e62cf8ad917ec8de7ad
Jose Enrique Ruiz Navarro <joseenriqueruiznavarro@gmail.com>
2017-03-03 13:44:32 +0100

    fixed loops and added winston

--- index.js
+++ index.js
@@ -2,41 +2,52 @@
 
 var request = require('request');
 var Promise = require('bluebird');
+var winston = require('winston');
 
 
-
-var doRequest = function (options) {
-	//console.log("Building promise");
-	return new Promise(function(resolve, reject){
-		request(options,function(error, response, body){
-			if(!error){
-				console.log(body);
-				resolve();
-			}else{
-				console.log(response||error);
-				reject(error);
-			}
-		})
-	});
+var doRequest = function(options) {
+    //console.log("Building promise");
+    return new Promise(function(resolve, reject) {
+        request(options, function(error, response, body) {
+            if (!error) {
+                if (response.statusCode == "404") {
+                    winston.info("Request failed " + response.statusCode)
+                }
+                resolve();
+            } else {
+                //winston.info(response || error)
+                console.log(response || error);
+                reject(error);
+            }
+        })
+    });
 }
 
 
-module.exports.doParallelRequest = function (url,method,number,body){
-	var options = {
-			  uri: url,
-			  method: method,
-			  json: true,
-				body: body,
-			};
+module.exports.doParallelRequest = function(url, method, number, body) {
+    if (method.toUpperCase() == "GET") {
+        var options = {
+            uri: url,
+            method: method,
+        };
+    } else {
+        var options = {
+            uri: url,
+            method: method,
+            json: true,
+            body: body,
+        };
+
+    }
 
 
-	var asyncTasks = [];
+    var asyncTasks = [];
 
-		for (var i = 0; i <= number; i++) {
-				asyncTasks.push(doRequest(options));
-			}
+    for (var i = 0; i < number; i++) {
+        asyncTasks.push(doRequest(options));
+    }
 
-return  Promise.all(asyncTasks);
+    return Promise.all(asyncTasks);
 
 
 }
