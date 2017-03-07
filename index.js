2a77553844a18ed528a3849fff45d61c2b082c6d
Jose Enrique Ruiz Navarro <joseenriqueruiznavarro@gmail.com>
2017-03-07 13:17:51 +0100

    added boolean result for result

--- index.js
+++ index.js
@@ -2,18 +2,20 @@
 
 var request = require('request');
 var Promise = require('bluebird');
-var winston = require('winston');
-
 
 var doRequest = function(options) {
     //console.log("Building promise");
     return new Promise(function(resolve, reject) {
+        var success = null;
         request(options, function(error, response, body) {
             if (!error) {
-                if (response.statusCode == "404") {
-                    winston.info("Request failed " + response.statusCode)
+                if (parseInt(response.statusCode) > 300) {
+                    success = false;
+                } else {
+                    success = true;
                 }
-                resolve();
+
+                resolve(success);
             } else {
                 //winston.info(response || error)
                 console.log(response || error);
@@ -40,12 +42,13 @@ module.exports.doParallelRequest = function(url, method, number, body) {
 
     }
 
-
     var asyncTasks = [];
 
     for (var i = 0; i < number; i++) {
         asyncTasks.push(doRequest(options));
+
     }
+
 
     return Promise.all(asyncTasks);
 
