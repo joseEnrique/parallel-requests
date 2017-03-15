2491eddb4eaac73441cb3eaa194ab7ae5d97bd18
Jose Enrique Ruiz Navarro <joseenriqueruiznavarro@gmail.com>
2017-03-15 18:30:15 +0100

    changes on structures

--- src/index.js
+++ src/index.js
@@ -1,13 +1,35 @@
+/*!
+parallel-requests 2.0.1, built on: 2017-03-15
+Copyright (C) 2017 Jose Enrique Ruiz Navarro
+http://www.isa.us.es/
+https://github.com/joseEnrique/parallel-requests
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.*/
+
+
 'use strict';
 
 var request = require('request');
 var Promise = require('bluebird');
+//var logger = require('./logger/logger');
+//var moment = require('moment');
 
 var doRequest = function(options) {
     //console.log("Building promise");
-    return new Promise(function(resolve, reject) {
+    return new Promise(function(resolve) {
         var success = null;
-        request(options, function(error, response, body) {
+        request(options, function(error, response) {
             if (!error) {
                 if (parseInt(response.statusCode) > 300) {
                     success = false;
@@ -20,9 +42,9 @@ var doRequest = function(options) {
                 success = false;
                 resolve(success);
             }
-        })
+        });
     });
-}
+};
 
 
 module.exports.doParallelRequest = function(url, method, number, body) {
@@ -53,10 +75,11 @@ module.exports.doParallelRequest = function(url, method, number, body) {
     return Promise.all(asyncTasks);
 
 
+};
+
+/*
+module.exports.doTestParallelFromfile() = function(path) {
+
+
 }
-
-
-module.exports.doTestParallelFromfile() = function(path){
-
-
-}
+*/
---
+++ src/logger/logger.js
@@ -0,0 +1,78 @@
+/*!
+parallel-requests 2.0.1, built on: 2017-03-15
+Copyright (C) 2017 Jose Enrique Ruiz Navarro
+http://www.isa.us.es/
+https://github.com/joseEnrique/parallel-requests
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.*/
+
+
+'use strict';
+
+/**
+ * Module dependecies.
+ * */
+
+var winston = require('winston');
+var config = require('../configurations/config');
+
+/**
+ * Configure here your custom levels.
+ * */
+var customLeves = {
+    levels: {
+        error: 7,
+        warning: 8,
+        custom: 9,
+        info: 12,
+        debug: 13
+    },
+    colors: {
+        error: 'red',
+        warning: 'yellow',
+        custom: 'magenta',
+        info: 'white',
+        debug: 'black'
+    }
+};
+
+winston.emitErrs = true;
+
+var logger = new winston.Logger({
+    levels: customLeves.levels,
+    colors: customLeves.colors,
+    transports: [
+        new winston.transports.File({
+            level: config.loglevel,
+            filename: config.logfile,
+            handleExceptions: true,
+            json: false,
+            maxsize: 5242880, //5MB
+            colorize: false
+        }),
+        new winston.transports.Console({
+            level: config.loglevel,
+            handleExceptions: true,
+            json: false,
+            colorize: true,
+            timestamp: true
+        })
+    ],
+    exitOnError: false
+});
+
+/*
+ * Export functions and Objects
+ */
+module.exports = logger;
